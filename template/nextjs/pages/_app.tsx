618ac51ebc66ecf56b31c3bc6a182ce4275609a1
Javi Adame <35896509+NickBlade@users.noreply.github.com>
2023-04-04 19:35:26 +0200

    feat: next font

--- template/nextjs/pages/_app.tsx
+++ template/nextjs/pages/_app.tsx
@@ -1,6 +1,19 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-sans",
+  display: "swap",
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <main className={inter.className}>
+        <Component {...pageProps} />
+      </main>
+    </>
+  )
 }
